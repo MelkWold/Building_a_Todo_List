86dc1229df12ff0670386d78af8c238866d84b39
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-10-05 19:32:46 -0400

    final updates and css changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,163 @@
 // Import
-import { useReducer, useState } from 'react';
-import './App.css'
-
+import { useReducer, useState } from "react";
+import "./App.css";
 
 // Define Action types
-const toDo = '';
 const ACTIONS = {
-    ADD_TO_DO: "ADD_TO_DO",
-    EDIT_TO_DO: "EDIT_TO_DO",
-    DELETE_TO_DO: "DELETE_TO_DO",
-    TOOGLE: "TOOGLE_TO_DO"
+  ADD_TO_DO: "ADD_TO_DO",
+  EDIT_TO_DO: "EDIT_TO_DO",
+  DELETE_TO_DO: "DELETE_TO_DO",
+  TOGGLE: "TOGGLE_TO_DO",
+  SAVE_TO_DO: "SAVE_TO_DO",
 };
 // Define reducer
-function toDoReducer (state, action){
-  switch(action.type){
-    case ACTIONS.ADD_TO_DO: 
+function toDoReducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TO_DO:
       // Copy the existing todo items and create a new to do object using the input. Generate unique ID (? Not sure if there is a better option) based to current timestamp
-        return [...state, { id:Date.now(), text: action.payload, completed: false, isEditing: false, },];
-    
-    case ACTIONS.TOOGLE_TO_DO: {
-      // Map over the todos list to find a todo list that matches the choice 
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: action.payload,
+          completed: false,
+          isEditing: false,
+        },
+      ];
+
+    case ACTIONS.TOGGLE_TO_DO: {
+      // Map over the state list to find a todo list that matches the choice
       // Create a copy of the todo
       // Toggle a to-do items check box
-        return todos.map((todo) => todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo );
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
     }
-       
+
     case ACTIONS.EDIT_TO_DO: {
-      // Map over the todos list to find a todo list that matches the choice
+      // Map over the state list to find a todo list that matches the choice
       // Create a copy of the todo
       // set isEditing to true to make the todo editable
-        return todos.map((todo) => todo.id === action.payload ? { ...todo, isEditing: true} : todo);
-      } 
-    
-    
-        
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, isEditing: true } : todo
+      );
+    }
+
+    case ACTIONS.SAVE_TO_DO:
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, text: action.payload.newText, isEditing: false }
+          : todo
+      );
+
     case ACTIONS.DELETE_TO_DO: {
-      // Loop through all todos and keeps those whose ID doesn't match the one passed by the action
-      return todos.filter((todo) => todo.id !== action.payload ); 
-      } 
-        default:
-        return state;
-
+      // Loop through all state and keeps those whose ID doesn't match the one passed by the action
+      return state.filter((todo) => todo.id !== action.payload);
+    }
+    default:
+      return state;
   }
-};
-
-
+}
 
 function App() {
-
   // Define the reducer function
-  const [toDos, dispatch] = useReducer(toDoReducer, []);
+  const [state, dispatch] = useReducer(toDoReducer, []);
   const [input, setInput] = useState("");
 
   // Handle adding a task
-function handleAddTask(){
-  // If users enter empty values don't do anything
-  if(input.trim() === '') return;
- // else, add the input provided to the list:
-  dispatch({ type: ACTIONS.ADD_TO_DO, payload: input });
-  // then, clear the input 
-  setInput("");
-}
+  function handleAddTask() {
+    // If users enter empty values don't do anything
+    if (input.trim() === "") return;
+    // else, add the input provided to the list:
+    dispatch({ type: ACTIONS.ADD_TO_DO, payload: input });
+    // then, clear the input
+    setInput("");
+  }
 
   return (
-    <>
-        <h1>Create Todo List</h1>
+    <div className = "mainContainer">
+      <h1>Create Todo List</h1>
       {/*Add input and Add Button*/}
-        <div>
-          <input type = "text" placeholder="Enter a task" value = {input} onChange = {(e) =>setInput(e.target.value)}/>
 
-          <button onClick={handleAddTask}>Add</button><br/>
-       </div>
+      <div className = "inputContainer">
+        <input className = "inputElement"
+          type="text"
+          placeholder="Enter a task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button onClick={handleAddTask}>Add</button>
+        <br />
+      </div>
 
       {/*Add the List of To do*/}
-       <ul>
-          { toDos.map((todo)=> (
-            <li key = {todo.id}>
-              <input type="checkbox" checked= {todo.completed} onChange={()=>dispatch({ type: ACTIONS.TOOGLE_TO_DO, payload: todo.id}) }/>
+      <ul>
+        {state.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() =>
+                dispatch({ type: ACTIONS.TOGGLE_TO_DO, payload: todo.id })
+              }
+            />
 
-              {todo.isEditing ? (
-                <>
+            {todo.isEditing ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={todo.text}
+                  id={`edit-${todo.id}`}
+                />
 
-                  <input type ="text" defaultValue = {todo.text} ud = {`edit-${todo.id}`} />
-
-                  <button onClick = { () => {
-                    const newText = document.getElementById(`edit-${todo.id}`).value;
-                    if (newText.trim() !== "") { dispatch ({ type: ACTIONS.SAVE_TO_DO, payload: { id: todo.id, newText }});
+                <button
+                  onClick={() => {
+                    const newText = document.getElementById(
+                      `edit-${todo.id}`
+                    ).value;
+                    if (newText.trim() !== "") {
+                      dispatch({
+                        type: ACTIONS.SAVE_TO_DO,
+                        payload: { id: todo.id, newText },
+                      });
                     }
-                  }}>Save</button>
-                </>
-              ): (
-                <>
-                  <span style = {{ textDecoration: todo.completed ? "none": "none", marginRight: "10px"}}>{todo.text}</span>
+                  }}
+                >
+                  Save
+                </button>
+              </>
+            ) : (
+              <>
+                <span>{todo.text}</span>
 
-                  <button onClick={ () => dispatch({ type: ACTIONS.EDIT_TO_DO, payload: todo.id })
-                  } diaabled = {!todo.completed}>Edit</button>
+                <button
+                  onClick={() =>
+                    dispatch({ type: ACTIONS.EDIT_TO_DO, payload: todo.id })
+                  }
+                  disabled={!todo.completed}
+                >
+                  Edit
+                </button>
 
-                  <button onClick = { () => dispatch({ type: ACTIONS.DELETE_TO_DO, payload: todo.id })} disabled = { !todo.completed }> Delete </button>
-                </>
-              )}
-              
-            </li>
-          ))}
-      </ul>            
-    </>
+                <button
+                  onClick={() =>
+                    dispatch({ type: ACTIONS.DELETE_TO_DO, payload: todo.id })
+                  }
+                  disabled={!todo.completed}
+                >
+                  {" "}
+                  Delete{" "}
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
-           
-
-export default App
+export default App;
